f8eb1ad5e365c365373abaf9c489824be826b4aa
Hector Niñez J <desarrolloPerrosky@gmail.com>
2019-06-08 00:30:43 -0400

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JugadorComponent } from './jugador/jugador.component';
+import { ListadoComponent } from './listado/listado.component';
+import { TableroComponent } from './tablero/tablero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JugadorComponent,
+    ListadoComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule
